3fad8ab0e4ebabc1212ef9f75d5a3f5924671ab6
Gafar H Salam <aygafsam2020@gmail.com>
2022-09-13 04:13:45 -0700

    Task 4. Updated

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,8 +11,12 @@
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 &&
-	    c >= 65 && c >= 90)
+	if ((c >= 97 && c <= 122) ||
+	   ( c >= 65 && c <= 90))
 		return (1);
+	else
+		return (0);
+	
+	_putchar('\n');
 	return (0);
 }
