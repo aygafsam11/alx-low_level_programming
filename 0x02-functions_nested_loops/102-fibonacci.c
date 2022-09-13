44274d80e5ee061ec87211f21f3988a3a7ee5ccc
Gafar H Salam <aygafsam2020@gmail.com>
2022-09-13 02:26:40 -0700

    Task 14. a program that prints the first 50 Fibonacci numbers, starting with 1 and 2

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the first 50 Fibonacci numbers
+ *		starting with 1 and 2 folled by a new line
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int count;
+	unsigned long fin1 = 0, fin2 = 1, sum;
+
+	for (count = 0; count < 50; ++count)
+	{
+		sum = fibn + fin2;
+		printf("%lu", sum);
+
+		fin1 = fin2;
+		fin2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
