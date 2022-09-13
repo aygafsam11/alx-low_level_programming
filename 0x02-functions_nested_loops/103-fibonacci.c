87a3275957e5c57b053701f264ce9f3a21ce76ce
Gafar H Salam <aygafsam2020@gmail.com>
2022-09-13 02:30:15 -0700

    Task 15. a program that finds and prints the sum of the even-valued terms

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the sum of even-valued
+ *		Fibonacci sequence not exceed
+ *		4million
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	unsigned long fin1 = 0, fin2 = 1, sum;
+	float total_sum;
+
+	while (1)
+	{
+		sum = fin1 + fin2;
+
+		if (sum > 4000000)
+			break;
+
+		if ((sum % 2) == 0)
+			total_sum += sum;
+
+		fin1 = fin2;
+		fibn = sum;
+	}
+	printf("%.0f\n", total_sum);
+
+	return (0);
+}
