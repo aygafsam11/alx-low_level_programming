385aa095192e21f0615e9eb20273014a65bafd3c
Gafar H Salam <aygafsam2020@gmail.com>
2022-09-13 02:53:09 -0700

    Task 16. Fibonacci series updated

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -45,12 +45,12 @@ int main(void)
 	{
 		if (f1o > 0)
 			printf("%lu", f1o);
-		initial0s = numLength(mx) - 1 - numLength(f1);
+		initial0 = numLength(mx) - 1 - numLength(f1);
 
-		while (f1o > 0 && initial0s > 0)
+		while (f1o > 0 && initial0 > 0)
 		{
 			printf("%d", 0);
-			--initial0s;
+			--initial0;
 		}
 
 		printf("%lu", f1);
