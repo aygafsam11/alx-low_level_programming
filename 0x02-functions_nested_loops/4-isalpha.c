770b6d97177622c2fb75a5d5f1a66acfd292c19c
Gafar H Salam <aygafsam2020@gmail.com>
2022-09-13 04:23:48 -0700

    Task 4. Updated and whitespace removed

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -12,10 +12,9 @@
 int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) ||
-	   ( c >= 65 && c <= 90))
+	   (c >= 65 && c <= 90))
 		return (1);
 	else
 		return (0);
-	_putchar ('\n');
 	return (0);
 }
