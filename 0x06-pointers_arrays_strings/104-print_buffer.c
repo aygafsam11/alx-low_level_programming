5861cb7537bff66a410eb0a57538a7eb0814d893
Gafar H Salam <aygafsam2020@gmail.com>
2022-09-21 11:00:42 -0700

    Task 12 Updated

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,42 +2,60 @@
 #include <stdio.h>
 
 /**
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
+ *
+ * Return: void
+ */
+
+void print_line(char *c, int s, int l)
+{
+	int j, k;
+
+	for (j = 0; j <= 9; j++)
+	{
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
+		else
+			printf("  ");
+		if (j % 2)
+			putchar(' ');
+	}
+	for (k = 0; k <= s; k++)
+	{
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
+		else
+			putchar('.');
+	}
+}
+
+/**
  * print_buffer - prints a buffer
- * @b: buffer.
- * @size: size of buffer.
- * Return: no return.
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
  */
 void print_buffer(char *b, int size)
 {
-	int j, k, l;
+	int i;
 
-	if (size <= 0)
-		printf("\n");
-	else
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
-		for (j = 0; j < size; j += 10)
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
 		{
-			printf("%.8x:", j);
-		= 0)
-					printf(" ");
-				if (k < size)
-					prin	for (k = j; k < j + 10; k++)
-			{
-				if (k % 2 =tf("%.2x", *(b + k));
-				else
-					printf("  ");
-			}
-			printf(" ");
-			for (l = j; l < j + 10; l+ + l) < 32 || *(b + l) > 126)
-					printf("%c", '.+)
-			{
-				if (l >= size)
-					break;
-				if (*(b');
-				else
-					printf("%c", *(b + l));
-			}
-			printf("\n");
+			print_line(b, 9, i);
 		}
+		else
+		{
+			print_line(b, size % 10 - 1, i);
+		}
+		putchar('\n');
 	}
+	if (size == 0)
+		putchar('\n');
 }
