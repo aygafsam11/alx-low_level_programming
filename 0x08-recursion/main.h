989a8be17cb068b973e99f73b9109736b5682cfb
Gafar H Salam <aygafsam2020@gmail.com>
2022-09-28 12:18:00 -0700

    Updating Main File Function Prototypes

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,6 +5,8 @@ int _putchar(char);
 void _puts_recursion(char *s);
 int square(int n, int val);
 int check_prime(int n, int resp);
+int last_index(char *s);
+int check(char *s, int start, int end, int pair);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
