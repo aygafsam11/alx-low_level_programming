41a99d2ed5ae1c5e360bb01a234b348ec06ce943
Gafar H Salam <aygafsam2020@gmail.com>
2022-10-27 13:21:37 -0700

    fixing

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,10 +10,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-        if (index >= (sizeof(unsigned long int) * BYTE_LENGHT))
-                return (-1);
+	if (index >= (sizeof(unsigned long int) * BYTE_LENGHT))
+		return (-1);
 
-        *n &= ~(1 << index);
+	*n &= ~(1 << index);
 
-        return (1);
+	return (1);
 }
