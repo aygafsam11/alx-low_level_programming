f5311ad7cb87e0932cd4801eb84de24101e874b6
Gafar H Salam <aygafsam2020@gmail.com>
2022-12-12 04:01:29 -0800

    Create the dynamic library libdynamic

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memset - concatenates two strings
+ * @s:  pointer
+ * @b: constant byte
+ * @n: n bytes of memory
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; n > 0 ; i++, n--)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
